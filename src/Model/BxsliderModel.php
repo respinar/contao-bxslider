17fc7426350bc0ce12b522a75e4dc3f1d3b32f2c
Hamid A. Peywasti <abbaszadeh.h@gmail.com>
2023-06-22 11:39:24 +0330

    models

---
+++ src/Model/BxsliderModel.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * bxSlider Extension for Contao Open Source CMS
+ *
+ * @copyright  Copyright (c) 2017, Respinar
+ * @author     Respinar <info@respinar.com>
+ * @license    https://opensource.org/licenses/lgpl-3.0.html LGPL
+ * @link       https://respinar.com/
+ */
+
+
+/**
+ * Namespace
+ */
+namespace Respinar\BxsliderBundle\Model;
+
+use Contao\Model;
+
+/**
+ * Class BxsliderModel
+ */
+class BxsliderModel extends Model
+{
+
+	/**
+	 * Name of the table
+	 * @var string
+	 */
+	protected static $strTable = 'tl_bxslider';
+
+}
