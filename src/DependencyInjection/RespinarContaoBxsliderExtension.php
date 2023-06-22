8a8e88e6623598d5fc8d401c5c564ab21e3205bb
Hamid A. Peywasti <abbaszadeh.h@gmail.com>
2023-06-22 11:39:08 +0330

    Bundle files

---
+++ src/DependencyInjection/RespinarContaoBxsliderExtension.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Contao bxSlider Bundle.
+ *
+ * (c) Hamid Abbaszadeh 2023 <abbaszadeh.h@gmail.com>
+ * @license MIT
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ * @link https://github.com/respinar/contao-bxslider-bundle
+ */
+
+namespace Respinar\BxsliderBundle\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+
+class RespinarContaoBxsliderExtension extends Extension
+{
+    /**
+     * @throws \Exception
+     */
+    public function load(array $configs, ContainerBuilder $container): void
+    {
+
+        $loader = new YamlFileLoader(
+            $container,
+            new FileLocator(__DIR__.'/../../config')
+        );
+
+        $loader->load('services.yaml');
+    }
+}
