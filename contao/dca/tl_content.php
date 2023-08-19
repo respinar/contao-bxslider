6090e24fbbc80fbe4ff49d54894e5b579474ca1c
Hamid A. Peywasti <abbaszadeh.h@gmail.com>
2023-08-19 17:55:10 +0330

    add thumbnail and cleanup

--- contao/dca/tl_content.php
+++ contao/dca/tl_content.php
@@ -9,15 +9,18 @@
  * @link       https://respinar.com/
  */
 
-use Contao\Backend;
+use Contao\System;
+use Contao\Controller;
+use Contao\BackendUser;
 
 /**
  * Add palettes to tl_content
  */
 $GLOBALS['TL_DCA']['tl_content']['palettes']['bxslider']   = '
-	{title_legend},type,headline;
-	{bx_slider_legend},bx_slider;
-	{template_legend},bx_slide_template,customTpl,size;
+	{title_legend},type,headline;	
+	{images_legend},bxSlider_items;
+	{bx_slider_legend},bxSlider,bxSlider_thumbnail;
+	{template_legend},bxSlider_template,customTpl,bxSlider_thumbnailSize;
 	{protected_legend:hide},protected;
 	{expert_legend:hide},guests,cssID,space';
 
@@ -25,38 +28,53 @@ $GLOBALS['TL_DCA']['tl_content']['palettes']['bxslider']   = '
 /**
  * Add fields to tl_content
  */
-$GLOBALS['TL_DCA']['tl_content']['fields']['bx_slider'] = array
+$GLOBALS['TL_DCA']['tl_content']['fields']['bxSlider'] = array
 (
-	'exclude'              => true,
-	'inputType'            => 'select',
-	'foreignKey'           => 'tl_bxslider.title',
-	'eval'                 => array('multiple'=>false, 'mandatory'=>true),
-	'sql'				   => "int(10) unsigned NOT NULL default '0'",
+	'exclude'          => true,
+	'inputType'        => 'select',
+	'foreignKey'       => 'tl_bxslider.title',
+	'eval'             => array('multiple'=>false, 'mandatory'=>true, 'tl_class'=>'w50'),
+	'sql'			   => "int(10) unsigned NOT NULL default '0'",
 );
 
-$GLOBALS['TL_DCA']['tl_content']['fields']['bx_slide_template'] = array
-(
-	'exclude'              => true,
-	'inputType'            => 'select',
-	'options_callback'     => array('tl_content_bxslider', 'getSlideTemplates'),
-	'eval'				   => array('tl_class'=>'w50 clr'),
-	'sql'				   => "varchar(64) NOT NULL default ''",
-);
 
-/**
- * Class tl_content_bxslider
- *
- * Provide miscellaneous methods that are used by the data configuration array.
- */
-class tl_content_bxslider extends Backend
-{
-	/**
-	 * Return all news templates as array
-	 *
-	 * @return array
-	 */
-	public function getSlideTemplates()
+
+$GLOBALS['TL_DCA']['tl_content']['fields']['bxSlider_template'] = array
+(
+	'exclude'          => true,
+	'inputType'        => 'select',
+	'options_callback' => static function ()
 	{
-		return $this->getTemplateGroup('bxslider_');
-	}
-}
+		return Controller::getTemplateGroup('bxslider_');
+	},
+	'eval'			   => array('tl_class'=>'w50 clr'),
+	'sql'			   => "varchar(64) NOT NULL default ''",
+);
+
+$GLOBALS['TL_DCA']['tl_content']['fields']['bxSlider_items'] = array
+(
+	'exclude'          => true,
+	'inputType'        => 'fileTree',
+	'eval'             => array('multiple'=>true, 'isGallery'=>true, 'extensions'=> '%contao.image.valid_extensions%', 'fieldType'=>'checkbox', 'orderField'=>'orderSRC', 'files'=>true),
+	'sql'              => "blob NULL",	
+);
+
+$GLOBALS['TL_DCA']['tl_content']['fields']['bxSlider_thumbnail'] = array
+(
+	'exclude'          => true,
+	'inputType'        => 'checkbox',
+	'eval'             => array('tl_class'=>'w50 m12'),
+	'sql'              => "char(1) COLLATE ascii_bin NOT NULL default ''"
+);
+
+$GLOBALS['TL_DCA']['tl_content']['fields']['bxSlider_thumbnailSize'] = array
+(
+	'exclude'                 => true,
+	'inputType'               => 'imageSize',
+	'reference'               => &$GLOBALS['TL_LANG']['MSC'],
+	'options_callback' => function () {
+		return System::getContainer()->get('contao.image.image_sizes')->getOptionsForUser(BackendUser::getInstance());
+	},
+	'eval'                    => array('rgxp'=>'natural', 'includeBlankOption'=>true, 'nospace'=>true, 'helpwizard'=>true, 'tl_class'=>'w50'),
+	'sql'                     => "varchar(128) COLLATE ascii_bin NOT NULL default ''"
+);
