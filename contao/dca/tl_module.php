887740d07e274989b73003528843f57ab24294a8
Hamid Peywasti <hamid.peywasti@gmail.com>
2023-08-27 10:11:38 +0330

    fix typo

--- contao/dca/tl_module.php
+++ contao/dca/tl_module.php
@@ -32,7 +32,7 @@ $GLOBALS['TL_DCA']['tl_module']['fields']['bxSlider'] = array
 	'inputType'            => 'select',
 	'foreignKey'           => 'tl_bxslider.title',
 	'eval'                 => array('multiple'=>false, 'includeBlankOption'=>true, 'tl_class'=>'w50'),
-	'sql'				   => "int(10) unsigned NOT NULL default '0'",
+	'sql'				   => "int(10) unsigned NOT NULL default 0",
 );
 
 $GLOBALS['TL_DCA']['tl_module']['fields']['bxSlider_items'] = array
